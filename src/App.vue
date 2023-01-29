0c9356aafa835784f472e8fc9c92ee1912da757a
wtsgitp <2876329298@qq.com>
2023-01-29 21:12:41 +0800

    nothing

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
    <h2>这是app根组件</h2>
    <!-- 在下方可以直接使用你们开发出来的组件查看效果,例如 ：-->
-   <w-header>标题</w-header>
-   <w-button>好好</w-button>
+   <!-- <w-header>标题</w-header>
+   <w-button>好好</w-button> -->
   </div>
 </template>
 
