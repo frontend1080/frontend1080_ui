013e7060f386fa40c611c271bba3930ffe28f830
一 丁 <970102437@qq.com>
2023-02-06 09:59:17 +0800

    示例文件

---
+++ src/components/dy-message/dy-message-view.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+     <button @click="showMessage">点击我</button>
+     <button @click="showMessage1">点击我</button>
+     <dy-icon name="icon-test44"></dy-icon>
+    </div>
+  </template>
+
+<script>
+import dyIcon from './components/dy-icon/dy-icon.vue'
+
+export default {
+  components: { dyIcon },
+  methods: {
+    showMessage () {
+      this.$message({
+        message: '我是一个message 消息提示',
+        duration: 3000,
+        autoClose: false,
+        position: 'right',
+        type: 'success'
+      })
+    },
+    showMessage1 () {
+      this.$message({
+        message: '我是一个message 消息提示',
+        duration: 3000,
+        autoClose: false,
+        position: 'center',
+        type: 'info'
+      })
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
